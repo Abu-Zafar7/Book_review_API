4f7f85e91f7a0e7b5ce48d65f69a87c7c840364b
casper-d07 <abuzafars99@gmail.com>
2024-12-20 20:26:04 +0530

    made changes to urls.py

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
+from django.conf import settings
 from .views import RegisterView, BookViewSet, ReviewViewSet, BookRecommendationView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from rest_framework.routers import DefaultRouter
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework.permissions import AllowAny
+
 schema_view = get_schema_view(
    openapi.Info(
       title="Book Reviews API",
@@ -23,15 +25,14 @@ router.register(r'books', BookViewSet)
 router.register(r'reviews', ReviewViewSet)
 
 urlpatterns = [
- 
     path('api/user/register/', RegisterView.as_view(), name='register'),
     path('api/user/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/user/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    
-    
     path('api/', include(router.urls)),
-
     path('api/recommendations/', BookRecommendationView.as_view(), name='book-recommendations'),
-    
-    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    ]
